4cb996001ef2c6aa321b1ef2df2cf1f2f3f99da1
nordicpioneer <67487638+nordicpioneer@users.noreply.github.com>
2020-11-29 17:44:35 +0100

    Henter filtall og mappestørrelser

---
+++ directorylogger-files-sizes.ps1
@@ -0,0 +1,10 @@
+$MriRaw = Get-ChildItem -Path '\Volumes\nice\p206\sleep_cog\MRI_RAW' -Directory
+$Results = foreach ($Folder in $MriRaw)
+    {
+    [PSCustomObject]@{
+        FolderName = $Folder.FullName
+        FileCount = ($Folder | Get-ChildItem -File -Recurse).count
+        FolderSizeGB = ("{0:N3} GB" -f (($Folder | Get-ChildItem -File -Recurse | Measure-Object -Property Length -sum).Sum /1GB))
+        }
+    }
+$Results|Export-Csv -Path '\Users\adrianvestol\001MRprosjekt\report-test_upload-filecount-foldersize-28-11-2020-0842.csv' -NoTypeInformation
